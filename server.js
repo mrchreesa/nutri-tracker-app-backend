ad3fde768ef251fd40d552f30ac1b9058824ea8e
Kristian Rahnev <KrisRah@kristians-mbp.lan>
2022-01-21 12:25:51 +0000

    cors update

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ require("dotenv").config();
 
 app.use(function (req, res, next) {
   //potential timeout problem with request.
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
   );
   next();
 });
